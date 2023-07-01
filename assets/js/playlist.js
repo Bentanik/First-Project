950cf4a082d9f18bcaab67f1e0d7143833b1ccef
Bentanik <kennyjamqw@gmail.com>
2023-07-01 17:21:17 +0700

    first

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -2,32 +2,32 @@ const playlist = [
   {
     id: 1,
     name: "Study Chill",
-    patch: "../assets/img/listthumb/Study Chill.png",
+    patch: "./assets/img/listthumb/Study Chill.png",
   },
   {
     id: 2,
     name: "Chill Mix",
-    patch: "../assets/img/listthumb/MixChill.jpg",
+    patch: "./assets/img/listthumb/MixChill.jpg",
   },
   {
     id: 3,
     name: "Romantic Mix",
-    patch: "../assets/img/listthumb/romanticmix.jpg",
+    patch: "./assets/img/listthumb/romanticmix.jpg",
   },
   {
     id: 4,
     name: "Trending",
-    patch: "../assets/img/listthumb/trending.jpg",
+    patch: "./assets/img/listthumb/trending.jpg",
   },
   {
     id: 5,
     name: "Ghibli",
-    patch: "../assets/img/listthumb/ghibli.webp",
+    patch: "./assets/img/listthumb/ghibli.webp",
   },
   {
     id: 6,
     name: "Work Hard",
-    patch: "../assets/img/listthumb/workhard.jpg",
+    patch: "./assets/img/listthumb/workhard.jpg",
   },
 ];
 
